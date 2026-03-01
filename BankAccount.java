da0a6274a7767e72e2e6170dc01b2f11803b5cb4
Tanuj Mytrean S <144891807+Stmytrean@users.noreply.github.com>
2026-03-01 16:16:00 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,29 @@
+public class BankAccount {
+    private String accountHolder;
+    private int accountNumber;
+    private double balance;
+
+    public BankAccount(String accountHolder, int accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Amount Deposited: " + amount);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Amount Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance!");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Current Balance: " + balance);
+    }
+}
